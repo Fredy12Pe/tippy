8e85830a46d5d4f2dc0cd520b0b9192da57dff11
Fredy <fapedro@uci.edu>
2020-01-16 21:06:45 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Fredy Pedro on 1/16/20.
+//  Copyright © 2020 Fredy Pedro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
